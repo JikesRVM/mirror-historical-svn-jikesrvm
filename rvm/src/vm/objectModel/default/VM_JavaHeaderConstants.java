4f04e15b3e00b9b351cb9e61fa98e713bc3d040b
David Grove <groved@us.ibm.com>
2002-03-26 15:47:57 +0000

    Initial Revision

---
+++ rvm/src/vm/objectModel/default/VM_JavaHeaderConstants.java
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$ 
+
+/**
+ * Constants for the JavaHeader. 
+ *
+ * @see VM_ObjectModel
+ * 
+ * @author David Bacon
+ * @author Steve Fink
+ * @author Dave Grove
+ */
+public interface VM_JavaHeaderConstants {
+
+  static final int JAVA_HEADER_END = -12;
+
+  static final int ARRAY_LENGTH_OFFSET = -4;
+
+  /** How many bits in the header are available for the GC and MISC headers? */
+  static final int NUM_AVAILABLE_BITS = VM_Collector.MOVES_OBJECTS ? 2 : 8;
+
+}
