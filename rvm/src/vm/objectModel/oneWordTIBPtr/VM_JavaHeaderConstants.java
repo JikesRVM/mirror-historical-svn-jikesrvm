4ff8f5e94b9f60b28ba2d313cda3426e8486676d
David Grove <groved@us.ibm.com>
2002-03-26 15:48:06 +0000

    Initial Revision

---
+++ rvm/src/vm/objectModel/oneWordTIBPtr/VM_JavaHeaderConstants.java
@@ -0,0 +1,27 @@
+/*
+ * (C) Copyright IBM Corp. 2001
+ */
+//$Id$ 
+
+/**
+ * Defines other header words not used for 
+ * core Java language support of memory allocation.
+ * Typically these are extra header words used for various
+ * kinds of instrumentation or profiling.
+ *
+ * @see VM_ObjectModel
+ * 
+ * @author David Bacon
+ * @author Steve Fink
+ * @author Dave Grove
+ */
+public interface VM_JavaHeaderConstants {
+
+  static final int JAVA_HEADER_END = -8;
+
+  static final int ARRAY_LENGTH_OFFSET = -4;
+
+  /** How many bits in the header are available for the GC and MISC headers? */
+  static final int NUM_AVAILABLE_BITS = 0;
+
+}
