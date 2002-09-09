66c13575fc616d23f84bd7f6fae1c3ad6a2a7259
Steve Blackburn <Steve.Blackburn@anu.edu.au>
2002-09-09 08:00:24 +0000

    Changed LG_WORDSIZE to LOG_WORDSIZE to be consistant with other constants

--- rvm/src/vm/memoryManagers/JMTk/vmInterface/ScanStatics.java
+++ rvm/src/vm/memoryManagers/JMTk/vmInterface/ScanStatics.java
@@ -52,7 +52,7 @@ public class ScanStatics
 	// slot contains a ref of some kind.  call collector specific
 	// processPointerField, passing address of reference
 	//
-	VM_Allocator.processPtrField(slots.add(slot << LG_WORDSIZE));
+	VM_Allocator.processPtrField(slots.add(slot << LOG_WORDSIZE));
 
       }  // end of for loop
 
@@ -87,7 +87,7 @@ public class ScanStatics
     boolean result = true;
     for ( int slot=0; slot<numSlots; slot++ ) {
       if ( ! VM_Statics.isReference(slot) ) continue;
-      VM_Address ref = VM_Address.fromInt(VM_Magic.getMemoryWord(slots.add(slot << LG_WORDSIZE)));
+      VM_Address ref = VM_Address.fromInt(VM_Magic.getMemoryWord(slots.add(slot << LOG_WORDSIZE)));
       if ( ! VM_ScanObject.validateRefs( ref, depth ) ) {
 	VM.sysWrite("ScanStatics.validateRefs: Bad Ref reached from JTOC slot ");
 	VM.sysWrite(slot,false);
@@ -106,7 +106,7 @@ public class ScanStatics
     VM.sysWrite("Dumping Static References...\n");
       for ( int slot=start; slot<last; slot++ ) {
 	if ( ! VM_Statics.isReference(slot) ) continue;
-	VM_Address ref = VM_Address.fromInt(VM_Magic.getMemoryWord(slots.add(slot << LG_WORDSIZE)));
+	VM_Address ref = VM_Address.fromInt(VM_Magic.getMemoryWord(slots.add(slot << LOG_WORDSIZE)));
 	if (!ref.isZero()) {
 	  VM.sysWrite(slot,false); VM.sysWrite(" "); VM_GCUtil.dumpRef(ref);
 	}
