aab42e499f2e80aec9731fdfbbcc01d71ab50b25
Daniel Frampton <Daniel.Frampton@anu.edu.au>
2009-02-04 03:20:34 +0000

    merge to 15279

--- workingMergeUp/rvm/src/org/jikesrvm/compilers/baseline/TemplateCompilerFramework.java
+++ workingMergeUp/rvm/src/org/jikesrvm/compilers/baseline/TemplateCompilerFramework.java
@@ -19,6 +19,7 @@ import org.jikesrvm.VM;
 import org.jikesrvm.Services;
 import org.jikesrvm.SizeConstants;
 import org.jikesrvm.adaptive.AosEntrypoints;
+import org.jikesrvm.classloader.ClassFileReader;
 import org.jikesrvm.classloader.RVMArray;
 import org.jikesrvm.classloader.BytecodeConstants;
 import org.jikesrvm.classloader.BytecodeStream;
@@ -1883,7 +1884,7 @@ public abstract class TemplateCompilerFramework
                 if (shouldPrint) asm.noteBytecode(biStart, "pseudo_load_int", value);
 
                 Offset offset = Offset.fromIntSignExtend(Statics.findOrCreateIntSizeLiteral(value));
-                emit_ldc(offset, RVMClass.CP_INT);
+                emit_ldc(offset, ClassFileReader.CP_INT);
 
                 break;
               }
@@ -1893,7 +1894,7 @@ public abstract class TemplateCompilerFramework
                 if (shouldPrint) asm.noteBytecode(biStart, "pseudo_load_long", value);
 
                 Offset offset = Offset.fromIntSignExtend(Statics.findOrCreateLongSizeLiteral(value));
-                emit_ldc2(offset, RVMClass.CP_LONG);
+                emit_ldc2(offset, ClassFileReader.CP_LONG);
 
                 break;
               }
@@ -1904,14 +1905,14 @@ public abstract class TemplateCompilerFramework
                   if (shouldPrint) asm.noteBytecode(biStart, "pseudo_load_word " + Integer.toHexString(value));
 
                   Offset offset = Offset.fromIntSignExtend(Statics.findOrCreateIntSizeLiteral(value));
-                  emit_ldc(offset, RVMClass.CP_INT);
+                  emit_ldc(offset, ClassFileReader.CP_INT);
                 } else {
                   long value = bcodes.readLongConst();
 
                   if (shouldPrint) asm.noteBytecode(biStart, "pseudo_load_word " + Long.toHexString(value));
 
                   Offset offset = Offset.fromIntSignExtend(Statics.findOrCreateLongSizeLiteral(value));
-                  emit_ldc2(offset, RVMClass.CP_LONG);
+                  emit_ldc2(offset, ClassFileReader.CP_LONG);
                   emit_l2i(); //dirty hack
                 }
                 break;
@@ -1922,7 +1923,7 @@ public abstract class TemplateCompilerFramework
                 if (shouldPrint) asm.noteBytecode(biStart, "pseudo_load_float", ibits);
 
                 Offset offset = Offset.fromIntSignExtend(Statics.findOrCreateIntSizeLiteral(ibits));
-                emit_ldc(offset, RVMClass.CP_FLOAT);
+                emit_ldc(offset, ClassFileReader.CP_FLOAT);
 
                 break;
               }
@@ -1932,7 +1933,7 @@ public abstract class TemplateCompilerFramework
                 if (shouldPrint) asm.noteBytecode(biStart, "pseudo_load_double", lbits);
 
                 Offset offset = Offset.fromIntSignExtend(Statics.findOrCreateLongSizeLiteral(lbits));
-                emit_ldc2(offset, RVMClass.CP_DOUBLE);
+                emit_ldc2(offset, ClassFileReader.CP_DOUBLE);
 
                 break;
               }
